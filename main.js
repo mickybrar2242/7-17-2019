d26a3976088e5277d030b46ad7ede1f311f40ced
Amitdeep Brar <754202@student.tor.loc>
2019-07-17 15:32:52 -0400

    lfourh commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const{app, BrowserWindow} = electron;
+
+let mainWindow;
+
+app.on('ready', function(){
+
+});
